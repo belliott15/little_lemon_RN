40a72c17699ebff23d9567c6f4a4ddd2cce31ce6
Beau Elliott <beauelliott@Beaus-MacBook-Pro.local>
2023-11-30 10:27:33 -0800

    finished design of welcome page

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,30 @@
 import React from "react";
-import { Pressable, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  Image,
+} from "react-native";
 
 const WelcomeScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={{ fontSize: "20rem" }}>Welcome to Little Lemon</Text>
-      <View style={styles.button}>
-        <Pressable onPress={() => navigation.navigate("Subscribe")}>
-          <Text>Subscribe</Text>
+      <Image
+        source={require("../assets/little-lemon-logo.png")}
+        resizeMode="contain"
+        style={styles.image}
+      />
+      <Text style={styles.welcomeText}>Welcome to Little Lemon</Text>
+      <Text style={{ fontSize: 18 }}>Your local bistro</Text>
+      <View style={styles.greeting}>
+        <Pressable
+          onPress={() => navigation.navigate("Subscribe")}
+          style={styles.button}
+        >
+          <Text style={{ color: "white", fontSize: 18 }}>Newsletter</Text>
         </Pressable>
       </View>
     </SafeAreaView>
@@ -22,17 +39,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   button: {
-    borderColor: "green",
-    borderWidth: "2px",
-    padding: 8,
-    backgroundColor: "#fff700",
-    borderRadius: "10px",
-    shadowColor: "#333333",
+    padding: 7,
+    backgroundColor: "#324d39",
+    borderRadius: 10,
+    shadowColor: "#b8bc86",
     shadowOffset: {
-      height: 6,
-      width: 6,
+      height: 8,
+      width: 8,
     },
     shadowOpacity: 0.6,
+    justifyContent: "center",
+    alignContent: "center",
+    width: 200,
+    alignItems: "center",
+  },
+  image: {
+    justifyContent: "center",
+    flex: 0.5,
+  },
+  greeting: {
+    justifyContent: "center",
+    position: "absolute",
+    bottom: 100,
+  },
+  welcomeText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginTop: 40,
   },
 });
 
